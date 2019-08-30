b255ba11f5fdeb18d8de3536966950ac2833d08f
carmenlab <cavagml@gmail.com>
2019-08-30 14:40:10 -0500

    Se agrego funcion leer archivo y mostrar links

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,31 @@
-const mdLinks = require('../');
+//Por ahora esta sera la funcion que nos diga si una ruta apunta a un archivo markdown o no
+const mdLinks = require('../index.js');
 
 
-describe('mdLinks', () => {
+describe('mdSearch', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should return true for a valid .md file', () => {
+    expect(mdLinks.mdSearch('../README.md')).toBe(true);
+    //console.log('FIX ME!');
   });
 
+  it('should return false for an invalid .md file', () => {
+    expect(mdLinks.mdSearch('../index.js')).toBe(false);
+    //console.log('FIX ME!');
+  });
+
+});
+
+describe('mdReadFile', () => {
+   
+  it('Should return all the links for the .md file', () => {
+    mdLinks.mdReadFile('../README.md',(data) => {
+      expect(data).toBe(linkFound);
+    });
+  });
+/*
+  it('should return the links', () => {
+    expect(mdLinks.mdReadFile('README.md')).toBe('https://es.wikipedia.org/wiki/Markdown');
+  });
+*/
 });
